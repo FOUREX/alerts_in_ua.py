462d424fcd30bfe0d8a66cb4b3e2c1d3ca8236f9
FOUREX <Foxtrotserega@gmail.com>
2023-02-09 12:06:52 +0200

    Finally

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup
 
-requirements = []
-with open('.alerts_in_ua.requirements.txt') as f:
-    requirements = f.read().splitlines()
 
 setup(
     name='alerts_in_ua.py',
     author='FOUREX',
     url='https://github.com/FOUREX/alerts_in_ua.py',
-    install_requires=requirements,
+    install_requires=[
+        'requests', 'aiohttp'
+    ],
 )
