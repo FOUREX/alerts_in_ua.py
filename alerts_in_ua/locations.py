2be2733dc851838c885505266ceef3ee7ea1830f
FOUREX <Foxtrotserega@gmail.com>
2023-02-10 02:30:39 +0200

    Змінено алгоритм фільтрування локацій. Додана можливість отримання списку атрибутів локацій звертаючись до списку локацій.
    Приклад:
    
    locations = Locations(...)  # Locations[Location(...), Location(...), Location(...)]
    locations_title = locations.location_title  # [..., ..., ...]

--- alerts_in_ua/locations.py
+++ alerts_in_ua/locations.py
@@ -1,4 +1,9 @@
+from typing import TypeVar
+
 from alerts_in_ua.location import Location
+
+
+Self = TypeVar("Self")
 
 
 class Locations(list):
@@ -11,22 +16,32 @@ class Locations(list):
 
         self.disclaimer = disclaimer
         self.last_updated_at = last_updated_at
-        self.type = "full"  # Ну потому что v1
+        self.type = "full"  # Ну тому що v1
+
+    def __getattr__(self, item) -> list:
+        return [getattr(location, item) for location in self]
 
     def append(self, __location: Location) -> None:
         super(Locations, self).append(__location)
 
-    def filter(self, **filters) -> list[Location]:
+    def remove(self, __location: Location) -> None:
+        super(Locations, self).remove(__location)
+
+    def filter(self, **filters) -> Self:
         """
         Повертає список місць які підпадають під вказані фільтри
 
         :param filters: Фільтри
         """
 
-        def location_filter(location: Location):
-            for _filter in filters:
-                if getattr(location, _filter) != filters[_filter]:
+        def location_filter(location: Location) -> bool:
+            for filter_name in filters:
+                if getattr(location, filter_name) != filters[filter_name]:
                     return False
             return True
 
-        return list(filter(location_filter, self))
+        return Locations(
+            *list(filter(location_filter, self)),
+            disclaimer=self.disclaimer,
+            last_updated_at=self.last_updated_at
+        )
