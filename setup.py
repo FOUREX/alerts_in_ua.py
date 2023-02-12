52719885446af3d6f1d2bb8d0a65839ea59494cb
FOUREX <Foxtrotserega@gmail.com>
2023-02-12 18:04:51 +0200

    Yep

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 from setuptools import setup
 
+from alerts_in_ua import __version__
+
+
+with open("README.md", "r", encoding="utf-8") as file:
+    long_description = file.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as file:
+    requires = file.read().splitlines()
+
 
 setup(
-    name='alerts_in_ua.py',
-    author='FOUREX',
-    url='https://github.com/FOUREX/alerts_in_ua.py',
-    install_requires=[
-        'requests', 'aiohttp'
-    ],
+    name="alerts_in_ua.py",
+    version=__version__,
+    description="Бібліотека для використання API сайту alerts.in.ua",
+    long_description=long_description,
+    author='FOUREX, SladkayaDoza',
+    author_email="Foxtrotserega@gmail.com",
+    url="https://github.com/FOUREX/alerts_in_ua.py",
+    license="MIT",
+    install_requires=requires,
 )
