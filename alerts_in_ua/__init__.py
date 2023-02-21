83340de658d8f0487f371f01ff384f1fe98b0fbd
FOUREX <Foxtrotserega@gmail.com>
2023-02-22 00:11:28 +0200

    Додано MapStyle

--- alerts_in_ua/__init__.py
+++ alerts_in_ua/__init__.py
@@ -4,6 +4,7 @@ from alerts_in_ua.alerts_client import AlertsClient as AlertsClient
 from alerts_in_ua.async_alerts_client import AsyncAlertsClient as AsyncAlertsClient
 from alerts_in_ua.location import Location
 from alerts_in_ua.locations import Locations
+from alerts_in_ua.map_style import MapStyle
 
 from alerts_in_ua.exceptions import (
     InvalidToken as InvalidToken,
@@ -12,10 +13,12 @@ from alerts_in_ua.exceptions import (
 )
 
 __all__: tuple[str] = (
+    "__version__",
     "AlertsClient",
     "AsyncAlertsClient",
     "Location",
     "Locations",
+    "MapStyle",
     "InvalidToken",
     "TooManyRequests",
     "UnknownError"
