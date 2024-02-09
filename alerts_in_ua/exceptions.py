f66f7116b42fc434a90f5008ed83c9c362ae4abc
FOUREX <Foxtrotserega@gmail.com>
2024-02-09 02:07:02 +0100

    Додане нове виключення Forbidden. Також для всіх виключень додані їхні коди помилок (error_code) та виведення повідомлення за замовчуванням

--- alerts_in_ua/exceptions.py
+++ alerts_in_ua/exceptions.py
@@ -1,16 +1,51 @@
-class InvalidToken(Exception):
-    """
-    Не дійсний токен
-    """
+class BaseAlertsException(Exception):
+    error_code: int
 
 
-class TooManyRequests(Exception):
+class NotAuthorized(BaseAlertsException):
     """
-    Перевищення ліміту запитів у хвилину (3-4 запити)
+    Токен API відсутній, неправильний, відкликаний або прострочений.
     """
 
+    error_code = 401
 
-class UnknownError(Exception):
+    def __init__(self):
+        super().__init__("Токен API відсутній, неправильний, відкликаний або прострочений.")
+
+
+class Forbidden(BaseAlertsException):
     """
-    Невідома помилка. Якщо у вас виникла ця помилка, то зв'яжіться з розробником. Telegram: @FOUREX_dot_py
+    Ваш IP адрес заблокований або API не доступне в вашій країні.
     """
+
+    error_code = 403
+    
+    def __init__(self):
+        super().__init__("Ваш IP адрес заблокований або API не доступне в вашій країні.")
+
+
+class TooManyRequests(BaseAlertsException):
+    """
+    Ліміт запитів на хвилину перевищено
+    """
+
+    error_code = 429
+    
+    def __init__(self):
+        super().__init__("Ліміт запитів на хвилину перевищено")
+
+
+class UnknownError(BaseAlertsException):
+    """
+    Невідома помилка. Якщо у вас виникла ця помилка, то зв'яжіться з розробником. Telegram: @FOUREX_dot_py,
+    Email: Foxtrotserega@gmail.com
+    """
+
+    def __init__(self, message: str = None):
+        if message is None:
+            self.message = "Невідома помилка. Якщо у вас виникла ця помилка, то зв'яжіться з розробником."\
+                           "Telegram: @FOUREX_dot_py, Email: Foxtrotserega@gmail.com"
+
+        self.message = message
+
+        super().__init__(self.message)
